6795cd722c3ed9ad11ea4eba65884265c7332d58
BlorZ <dupont.david21@hotmail.fr>
2018-12-19 15:40:10 +0100

    fix erreur bdd

--- Entities/src/entities/Autre_entity.java
+++ Entities/src/entities/Autre_entity.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @Entity
 public class Autre_entity extends Vehicule_entity implements Serializable {
 	
-	private static final long serialVersionUID = 6124179836989716436L;
+	private static final long serialVersionUID = 1L;
 
 	public Autre_entity() {
 		super();
